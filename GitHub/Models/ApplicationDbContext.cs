f1451f47023903bb661174cdcd1158518d4b4430
bereketbruk <bereketbruk@outlook.com>
2018-09-04 12:21:42 +0200

    create the gig model

---
+++ GitHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GitHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genere> Generes { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ GitHub/Models/Gig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GitHub.Models
+{
+    public class Gig
+    {
+        // we wanna make sure that id is not nullable
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venu { get; set; }
+
+        [Required]
+        public Genere Genere { get; set; }
+    }
+}
